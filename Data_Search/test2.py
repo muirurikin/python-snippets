a7ad432733cae90dbce9ebaec66f9edd12d81982
muirurikin <muirurikin@gmail.com>
2016-07-29 11:50:52 +0300

    update files

--- Data_Search/test2.py
+++ Data_Search/test2.py
@@ -1,11 +1,11 @@
-import csv
+import csv  # import module
 
+with open('data.csv') as csvfile:  # read csv file
+    your_id = raw_input("Enter id: ") # Prompt  user
+    readCSV = csv.reader(csvfile, delimiter=',')  # read the file
 
-with open('data.csv') as csvfile:
-        your_id = int(raw_input("Enter id:"))
-	readCSV = csv.reader(csvfile ,delimiter=',')
-	print(readCSV)
-	
-	for id in readCSV:
-		print (id)
-
+    for id_ in readCSV:
+        if your_id == id_[0]:
+            print 'First Name: ' + id_[1] + 'Last Name: ' + id_[2] + 'Email Address: ' + id_[3] + 'Gender: ' + id_[4]
+        else:
+            pass
